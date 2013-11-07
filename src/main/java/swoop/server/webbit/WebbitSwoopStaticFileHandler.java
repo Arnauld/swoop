81c8b17982fca4bf7163bea8e40d5489bf01c9f1
Arnauld Loyer <arnauld.loyer@gmail.com>
2013-11-07 22:30:44 +0100

    update version + fix dependencies issue + fix webbit integration

--- src/main/java/swoop/server/webbit/WebbitSwoopStaticFileHandler.java
+++ src/main/java/swoop/server/webbit/WebbitSwoopStaticFileHandler.java
@@ -8,7 +8,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 import java.util.concurrent.Executor;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -19,8 +23,10 @@ import org.webbitserver.HttpHandler;
 import org.webbitserver.HttpRequest;
 import org.webbitserver.HttpResponse;
 import org.webbitserver.handler.AbstractResourceHandler;
+import org.webbitserver.handler.FileEntry;
 import org.webbitserver.handler.StaticFileHandler;
 
+import org.webbitserver.helpers.ClassloaderResourceHelper;
 import swoop.ResourceContent;
 import swoop.ResourceHandler;
 import swoop.StatusCode;
@@ -44,7 +50,7 @@ public class WebbitSwoopStaticFileHandler implements HttpHandler {
     private final InternalResourceHandler delegate;
 
     public WebbitSwoopStaticFileHandler(RouteRegistry routeRegistry, Executor ioThread) {
-        this.delegate = new InternalResourceHandler(ioThread);
+        this.delegate = new InternalResourceHandler(ioThread, 0);
         this.routeRegistry = routeRegistry;
     }
 
@@ -99,9 +105,12 @@ public class WebbitSwoopStaticFileHandler implements HttpHandler {
     }
 
     class InternalResourceHandler extends AbstractResourceHandler implements ResourceHandler {
-        
-        public InternalResourceHandler(Executor ioThread) {
+
+        private long maxAge;
+
+        public InternalResourceHandler(Executor ioThread, long maxAge) {
             super(ioThread);
+            this.maxAge = maxAge;
         }
         
         @Override
@@ -109,7 +118,7 @@ public class WebbitSwoopStaticFileHandler implements HttpHandler {
             HttpRequest request = context.get(HttpRequest.class);
             HttpResponse response = context.get(HttpResponse.class);
             HttpControl control = context.get(HttpControl.class);
-            ioThread.execute(new FileWorker(dir, path, request, response, control));
+            ioThread.execute(new FileWorker(dir, path, request, response, control, maxAge));
         }
 
         @Override
@@ -117,52 +126,134 @@ public class WebbitSwoopStaticFileHandler implements HttpHandler {
                 HttpControl control) {
             String path = (String)request.data(ResourceContent.RESOURCE_PATH);
             File dir = (File)request.data(ResourceContent.RESOURCE_DIR);
-            return new FileWorker(dir, path, request, response, control);
+            return new FileWorker(dir, path, request, response, control, maxAge);
         }
 
         protected class FileWorker extends IOWorker {
-            private File dir;
-            private File file;
 
-            private FileWorker(File dir, String path, HttpRequest request, HttpResponse response, HttpControl control) {
-                super(path, request, response, control);
-                this.dir = dir;
-            }
+                private File file;
 
-            @Override
-            protected boolean exists() throws IOException {
-                file = resolveFile(path);
-                return file != null && file.exists();
-            }
+                private final HttpResponse response;
 
-            @Override
-            protected ByteBuffer fileBytes() throws IOException {
-                return file.isFile() ? read(file) : null;
-            }
+                private final HttpRequest request;
 
-            @Override
-            protected ByteBuffer welcomeBytes() throws IOException {
-                File welcome = new File(file, welcomeFileName);
-                return welcome.isFile() ? read(welcome) : null;
-            }
+            private final File dir;
+            private final String path;
+            private final long maxAge;
 
-            private ByteBuffer read(File file) throws IOException {
-                return read((int) file.length(), new FileInputStream(file));
-            }
-
-            private File resolveFile(String path) throws IOException {
-                // Find file, relative to roo
-                File result = new File(dir, path).getCanonicalFile();
-
-                // For security, check file really does exist under root.
-                String fullPath = result.getPath();
-                if (!fullPath.startsWith(dir.getCanonicalPath() + File.separator)
-                        && !fullPath.equals(dir.getCanonicalPath())) {
-                    // Prevent paths like http://foo/../../etc/passwd
-                    return null;
+                private String mimeType(String uri) {
+                    String ext = uri.lastIndexOf(".") != -1 ? uri.substring(uri.lastIndexOf(".")) : null;
+                    String currentMimeType = mimeTypes.get(ext);
+                    if (currentMimeType == null) currentMimeType = "text/plain";
+                    return currentMimeType;
                 }
-                return result;
+                //based on: http://m2tec.be/blog/2010/02/03/java-md5-hex-0093
+                private  String MD5(String md5) {
+                    try {
+                        java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+                        byte[] array = md.digest(md5.getBytes("UTF-8"));
+                        StringBuffer sb = new StringBuffer();
+                        for (int i = 0; i < array.length; ++i) {
+                            sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+                        }
+                        return sb.toString();
+                    } catch (Exception e) {
+                        return null;
+                    }
+                }
+
+                private String toHeader(Date date) {
+                    SimpleDateFormat httpDateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+                    httpDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+                    return httpDateFormat.format(date);
+                }
+
+                private Date fromHeader(String date) {
+                    SimpleDateFormat httpDateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+                    httpDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+                    try {
+                        return httpDateFormat.parse(date);
+                    } catch (Exception ex) {
+                        return new Date();
+                    }
+                }
+
+                protected FileWorker(File dir, String path, HttpRequest request, HttpResponse response, HttpControl control, long maxAge) {
+                    super(request.uri(), request, response, control);
+                    this.dir = dir;
+                    this.path = path;
+                    this.maxAge = maxAge;
+                    this.response = response;
+                    this.request = request;
+                }
+
+                @Override
+                protected boolean exists() throws IOException {
+                    file = resolveFile(path);
+                    return file != null && file.exists();
+                }
+
+                @Override
+                protected boolean isDirectory() throws IOException {
+                    return file.isDirectory();
+                }
+
+                @Override
+                protected byte[] fileBytes() throws IOException {
+                    byte[] raw = file.isFile() ? read(file) : null;
+                    //add cache control headers if needed
+                    if (raw != null) {
+                        Date lastModified = new Date(file.lastModified());
+                        String hashtext = MD5(Long.toString(lastModified.getTime()));
+                        if (hashtext != null) response.header("ETag", "\"" + hashtext + "\"");
+
+                        response.header("Last-Modified", toHeader(lastModified));
+                        //is there an incoming If-Modified-Since?
+                        if (request.header("If-Modified-Since") != null) {
+                            if (fromHeader(request.header("If-Modified-Since")).getTime() >= lastModified.getTime() ) {
+                                response.status(304);
+                            }
+                        }
+                        //is setting cache control necessary?
+                        if (maxAge != 0) {
+                            response.header("Expires", toHeader( new Date(new Date().getTime() + maxAge * 1000)));
+                            response.header("Cache-Control", "max-age=" + maxAge+", public");
+                        }
+                    }
+                    return raw;
+                }
+
+                @Override
+                protected byte[] welcomeBytes() throws IOException {
+                    File welcome = new File(file, welcomeFileName);
+                    return welcome.isFile() ? read(welcome) : null;
+                }
+
+                @Override
+                protected byte[] directoryListingBytes() throws IOException {
+                    if (!isDirectory()) {
+                        return null;
+                    }
+                    Iterable<FileEntry> files = ClassloaderResourceHelper.fileEntriesFor(file.listFiles());
+                    return directoryListingFormatter.formatFileListAsHtml(files);
+                }
+
+                private byte[] read(File file) throws IOException {
+                    return read((int) file.length(), new FileInputStream(file));
+                }
+
+                protected File resolveFile(String path) throws IOException {
+                    // Find file, relative to root
+                    File result = new File(dir, path).getCanonicalFile();
+
+                    // For security, check file really does exist under root.
+                    String fullPath = result.getPath();
+                    if (!fullPath.startsWith(dir.getCanonicalPath() + File.separator) && !fullPath.equals(dir.getCanonicalPath())) {
+                        // Prevent paths like http://foo/../../etc/passwd
+                        return null;
+                    }
+                    return result;
+                }
             }
-        }
     }
 }
